3013ec45144d6abf30b9e967f99722daf1a7138d
iidaissei <issei.288.higashi@icloud.com>
2021-11-12 15:19:35 +0900

    Fix: 軽微な修正

--- yumeko_test/src/yumeko_inspection.py
+++ yumeko_test/src/yumeko_inspection.py
@@ -9,11 +9,13 @@
 import sys
 import rospy
 import roslib
+import actionlib
 import smach
 import smach_ros
 
-from std_msgs.msg import String
+from std_msgs.msg import String, Float64
 from happymimi_navigation.srv import NaviLocation
+from enter_room.srv import EnterRoom
 from happymimi_voice_msgs.srv import TTS, YesNo, ActionPlan
 from actplan_executor.msg import APExecutorAction, APExecutorGoal
 
@@ -30,7 +32,7 @@ class Enter(smach.State):
     def execute(self, userdata):
         rospy.loginfo("Executing state: ENTER")
         tts_srv("Start GPSR")
-        self.enter_srv(0.8)
+        self.enter_srv(distance=0.8, velocity=0.2)
         return 'enter_finish'
 
 
@@ -44,20 +46,20 @@ class DecideMove(smach.State):
         # Subscriber
         self.posi_sub = rospy.Subscriber('/current_location', String, self.crPosiCB)
         # Value
-        self.current_position = 'none'
+        self.current_loc = 'none'
         self.cmd_limit = 3
 
     def crPosiCB(self, data):
-        self.current_position = data.data
+        self.current_loc = data.data
 
     def execute(self, userdata):
         rospy.loginfo('Executing state: DECIDE_MOVE')
         if userdata.cmd_count_in == self.cmd_limit + 1:
-            tts_srv("Finish all command, Move to exit")
+            tts_srv("Finish all command, Move to entrance")
             self.navi_srv('entrance')
             tts_srv("Finish GPSR")
             return 'all_cmd_finish'
-        elif self.current_position != 'operator':
+        elif self.current_loc != 'operator':
             self.navi_srv('operator')
             return 'decide_finish'
         else:
@@ -71,32 +73,46 @@ class ListenCommand(smach.State):
                                          'listen_failure',
                                          'next_cmd'],
                              input_keys = ['cmd_count_in'],
-                             output_keys = ['cmd_out',
+                             output_keys = ['cmd_out_action',
+                                            'cmd_out_data',
                                             'cmd_count_out'])
         # Publisher
         self.head_pub = rospy.Publisher('/servo/head', Float64, queue_size = 1)
         # ServiceProxy
-        self.listen_srv = rospy.ServiceProxy('/gpsr/actionplan', ActionPlan)
+        # self.listen_srv = rospy.ServiceProxy('/gpsr/actionplan', ActionPlan)
+        self.listen_srv = rospy.ServiceProxy('/planning_srv', ActionPlan)
         self.yesno_srv = rospy.ServiceProxy('/yes_no', YesNo)
         # Value
         self.listen_count = 1
+        # kimeuti actplan
+        self.apdemo_action = [['go','grasp','go','give'],
+                        ['go','grasp','go','give'],
+                        ['go','grasp','go','give']]
+        self.apdemo_data = [['cupboard','black coffee','operator','black coffee'],
+                       ['cupboard','red cup','operator','red cup'],
+                       ['cupboard','green snack','operator','green snack']]
 
     def execute(self, userdata):
         rospy.loginfo("Executing state: LISTEN_COMMAND")
         cmd_count = userdata.cmd_count_in
-        self.head_pub(20)
+        self.head_pub.publish(0)
         if self.listen_count <= 3:
             tts_srv("CommandNumber is " + str(cmd_count))
             tts_srv("ListenCount is " + str(self.listen_count))
             tts_srv("Please instruct me")
-            result = self.listen_srv().result
-            if result:
+            actplan_res = self.listen_srv()
+            # actplan_res = True
+            if actplan_res.result:
+            # if actplan_res:
                 tts_srv("Is this correct?")
                 answer = self.yesno_srv().result
                 if answer:
+                    userdata.cmd_out_action = actplan_res.action
+                    userdata.cmd_out_data = actplan_res.data
+                    # userdata.cmd_out_action = self.apdemo_action[cmd_count - 1]
+                    # userdata.cmd_out_data = self.apdemo_data[cmd_count -1]
                     self.listen_count = 1
                     cmd_count += 1
-                    userdata.cmd_out = result
                     userdata.cmd_count_out = cmd_count
                     return 'listen_success'
                 else:
@@ -120,7 +136,8 @@ class ExeAction(smach.State):
         smach.State.__init__(self,
                              outcomes = ['action_success',
                                          'action_failure'],
-                             input_keys = ['cmd_in'])
+                             input_keys = ['cmd_in_action',
+                                           'cmd_in_data'])
 
     def execute(self, userdata):
         rospy.loginfo("Executing state: EXE_ACTION")
@@ -128,8 +145,8 @@ class ExeAction(smach.State):
         ac.wait_for_server()
         # Goalの生成
         goal = APExecutorGoal()
-        goal.action = userdata.cmd_in.action
-        goal.data = userdata.cmd_in.data
+        goal.action = userdata.cmd_in_action
+        goal.data = userdata.cmd_in_data
         print goal
         ac.send_goal(goal)
         ac.wait_for_result()
@@ -165,7 +182,8 @@ if __name__ == '__main__':
                 transitions = {'listen_success':'EXE_ACTION',
                                'listen_failure':'LISTEN_COMMAND',
                                'next_cmd':'DECIDE_MOVE'},
-                remapping = {'cmd_out':'cmd',
+                remapping = {'cmd_out_action':'ap_action',
+                             'cmd_out_data':'ap_data',
                              'cmd_count_in':'cmd_count',
                              'cmd_count_out':'cmd_count'})
 
@@ -174,6 +192,7 @@ if __name__ == '__main__':
                 ExeAction(),
                 transitions = {'action_success':'DECIDE_MOVE',
                                'action_failure':'DECIDE_MOVE'},
-                remapping = {'cmd_in':'cmd'})
+                remapping = {'cmd_in_action':'ap_action',
+                             'cmd_in_data':'ap_data'})
 
     outcome = sm_top.execute()
