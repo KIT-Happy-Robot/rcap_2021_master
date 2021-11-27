c95fa7957bda62e53008d5a6dab0e266a6a5d01c
iidaissei <issei.288.higashi@icloud.com>
2021-11-27 17:00:46 +0900

    update: final_demo

--- final_demo/src/fd_master.py
+++ final_demo/src/fd_master.py
@@ -11,7 +11,7 @@ import roslib
 import sys
 from happymimi_navigation.srv import NaviLocation
 from happymimi_msgs.srv import StrTrg
-from happymimi_manipulation_msgs.srv import RecognitionToGrasping
+from happymimi_manipulation_msgs.srv import RecognitionToGrasping, RecognitionToGraspingResponse
 path = roslib.packages.get_pkg_dir('happymimi_teleop') + '/src/'
 sys.path.insert(0, path)
 from base_control import BaseControl
@@ -28,36 +28,43 @@ class FinalDemo():
         # Value
         self.grasp_result = False
         self.rotate = 90 #####
+        self.execute()
 
     def execute(self):
         rospy.loginfo('Executing: FinalDemo')
-        self.tts_srv('Start Demonstration')
+        self.tts_srv('/fd/start_fd')
         rospy.sleep(0.5)
-        self.navi_srv('location') #####
+        self.navi_srv('Tall table') #####
         rospy.sleep(0.5)
         grasp_count = 0
-        self.tts_srv('Grasping an object')
+        self.tts_srv('/fd/grasp_start')
         while not rospy.is_shutdown():
             rospy.sleep(0.3)
-            self.grasp_result = self.grasp_srv('any').result #####
+            self.grasp_result = self.grasp_srv(RecognitionToGraspingResponse('any')).result #####
             if self.grasp_result == False:
                 if grasp_count >= 3: #####
-                    self.tts_srv("Sorry I couldn't grasp it.")
+                    self.tts_srv("/fd/grasp_failed")
                     break
                 else:
-                    self.tts_srv("Grasp failed. I'll try again.")
+                    self.tts_srv("/fd/grasp_retry")
                 grasp_count += 1
             else:
-                self.tts_srv('Successfully grasped.')
+                self.tts_srv('/fd/grasp_success')
                 break
+        rospy.sleep(0.5)
+        #self.bc.rotateAngle(self.rotate)
+        self.navi_srv('operator')
+        rospy.sleep(0.5)
+        self.tts_srv('/fd/give_object')
+        self.arm_srv('give')
+        rospy.sleep(0.5)
+        #self.tts_srv('/fd/return_table')
+        self.bc.rotateAngle(-self.rotate)
+        self.navi_srv('Tall table')
         rospy.sleep(0.5)
         self.bc.rotateAngle(self.rotate)
         rospy.sleep(0.5)
-        self.tts_srv('Here you are')
-        self.arm_srv('give')
-        self.bc.rotateAngle(-self.rotate)
-        rospy.sleep(0.5)
-        self.tts_srv('Finish Demonstration. Thank you very much')
+        self.tts_srv('/fd/finish_fd')
 
 if __name__ == '__main__':
     rospy.init_node('fd_master')
